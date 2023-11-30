732a6e32dc510099cd4ddd37904146dc7fd739d3
Alexander Ivashin <studyvashin@gmail.com>
2023-11-30 12:50:06 +1000

    Добавьте файлы проекта.

---
+++ ECDSA/Program.cs
@@ -0,0 +1,16 @@
+﻿using EllipticCurve;
+
+//Генерация ключевой пары
+PrivateKey privateKey = new PrivateKey();
+PublicKey  publicKey  = privateKey.publicKey();
+
+string message = "string message";
+
+//Генерация подписи
+Signature signature = Ecdsa.sign(message, privateKey);
+
+//Base64 представление подписи
+Console.WriteLine("Singature: " + signature.toBase64());
+
+//Верификация подписи
+Console.WriteLine("Is verified: " + Ecdsa.verify(message,signature, publicKey));
